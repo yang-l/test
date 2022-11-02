30698e209cfad7af5ea805377cb2b26198c09921
Y@L <Y@L>
2022-11-02 19:14:06 +1300

    Passing build number

--- tf/variables.tf
+++ tf/variables.tf
@@ -1,13 +1,19 @@
 variable "service_name" {
-  type        = string
-  description = "Naming convention prefix"
-  default     = "server"
+  type    = string
+  default = "server"
 }
 
 variable "vpc_cidr_base" {
+  type    = string
   default = "10.0"
 }
 
 variable "az_count" {
+  type    = string
   default = "3"
+}
+
+variable "port" {
+  type    = number
+  default = 8080
 }
--- tf/vpc.tf
+++ tf/vpc.tf
@@ -80,3 +80,39 @@ resource "aws_route_table_association" "priv_sub" {
   subnet_id      = element(aws_subnet.priv_sub.*.id, count.index)
   route_table_id = element(aws_route_table.priv.*.id, count.index)
 }
+
+resource "aws_security_group" "alb" {
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ecs" {
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
